9e4a8482e72f82d18c4f3d40bd1b4d46642eda00
Marina Kukso <marina.kukso@gmail.com>
2016-02-24 23:09:42 -0800

    rect grows using requestanimationframe

--- browser.js
+++ browser.js
@@ -1,15 +1,32 @@
 var createElement = require('svg-create-element');
- 
+
 var svg = createElement('svg');
 svg.setAttribute("height", "100%")
 svg.setAttribute("width", "100%")
-svg.appendChild(createElement('rect', {
-  fill: 'SaddleBrown',
-  stroke: 'olive',
-  strokeWidth: 4,
-  x: 10,
-  y: 10,
-  width: 100,
-  height: 200,
-}));
+var w = 100;
+var h = 200;
+var rect = 
+  createElement('rect', {
+    id: 'trunk',
+    fill: 'SaddleBrown',
+    stroke: 'olive',
+    strokeWidth: 4,
+    x: 100,
+    y: 100,
+    width: w,
+    height: h,
+  });
+svg.appendChild(rect)
 document.body.appendChild(svg);
+
+function hwgrow (){
+  if (w<300 && h<600){
+    h++;
+    w++;
+    rect.setAttribute("width", w);
+    rect.setAttribute("height", h);
+    window.requestAnimationFrame(hwgrow)
+  }
+}
+
+window.requestAnimationFrame(hwgrow)
