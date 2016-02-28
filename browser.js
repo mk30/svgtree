1f63d9625f634663b4cd14d8988b21486c58568d
Marina Kukso <marina.kukso@gmail.com>
2016-02-27 16:03:11 -0800

    basic hyperscript example working

--- browser.js
+++ browser.js
@@ -1,59 +1,10 @@
-var createElement = require('svg-create-element');
-var svg = createElement('svg');
-svg.setAttribute("height", "100%")
-svg.setAttribute("width", "100%")
-var trunkw = 10;
-var trunkh = 20;
-var trunkx = 100;
-var trunky = 400;
-var rect = 
-  createElement('rect', {
-    id: 'trunk',
-    fill: 'SaddleBrown',
-    stroke: 'olive',
-    strokeWidth: 4,
-    x: trunkx,
-    y: trunky,
-    width: trunkw,
-    height: trunkh,
-  });
-var branch = 
-  createElement('rect', {
-    id: 'branch',
-    fill: 'SaddleBrown',
-    stroke: 'olive',
-    strokeWidth: 4,
-    x: trunkx-4,
-    y: 400 -trunkh,
-    width: trunkw,
-    height: trunkh,
-  });
-svg.appendChild(rect)
-document.body.appendChild(svg);
+var createElement = require('virtual-dom/create-element');
+var h = require('virtual-hyperscript-svg');
 
-function trgrow (){
-  if (trunkw<150 && trunkh<300){
-    trunkh = trunkh + 5;
-    trunkw++;
-    trunky = 400 - trunkh;
-    rect.setAttribute("width", trunkw);
-    rect.setAttribute("height", trunkh);
-    rect.setAttribute("y", trunky);
-    window.requestAnimationFrame(trgrow)
-  }
-};
-function brgrow() {
-  if (!document.getElementById('branch')) {
-    svg.appendChild(branch);
-    document.body.appendChild(svg)
-    window.requestAnimationFrame(brgrow)
-  }
-  else {
-      branch.setAttribute("width", trunkw/2);
-      branch.setAttribute("height", trunkh);
-      branch.setAttribute("y", 400- trunkh*2 );
-      window.requestAnimationFrame(brgrow)
-    }
-};
-window.requestAnimationFrame(trgrow)
-window.requestAnimationFrame(brgrow);
+var fn = function (){
+  return h('svg', { width: '100%', height: '100%' }, [
+    h('rect', { fill: 'red', x: 50, y: 50, width: 300, height: 100 })
+  ]);
+}
+var svg = createElement(fn())
+document.body.appendChild(svg);
