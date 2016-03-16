2193889759278a34b8a948c07550f74bbbd4819f
Marina Kukso <marina.kukso@gmail.com>
2016-03-16 11:56:42 -0700

    branches now grow out of the top of main trunk

--- browser.js
+++ browser.js
@@ -19,11 +19,11 @@ function render (state){
 }
 
 function recur (depth, state) {
-  var m = 400
-  var n =25 
   var dd = (state.time - state.inittime)/500
-  var height = Math.min(400, state.height*dd)
-  if (depth >= Math.min(5, dd)){
+  var height = Math.min(200, state.height*dd)
+  var x = 400;
+  var y = 400-height
+  if (depth >= Math.min(2, dd)){
     return '' 
   }
   else  return  h('g', {
@@ -31,33 +31,21 @@ function recur (depth, state) {
       h('rect', { 
         fill: 'none',
         stroke: 'olive',
-        x: m, 
-        y: 400-height, 
+        x: x, 
+        y: y, 
         width: state.width, 
-        //at beginning should be min state.height, as
-        //approaches end (over time), should go at a fixed
-        //rate until depth stops time)
         height: height
-        //Math.max(state.height*dd, 200)
-        ///1000*state.height),
-        //Math.max(0, state.height - dd)
-        //n/10*dd,
-
       }), 
       h('g',{transform: 
-        'translate(-'+m/6+',-'+n/2+
-          ') scale(1) rotate(-12 '+m+' '+n+')'
+        'translate(0,0) scale(1) rotate(-225 '+x+' '+y+')'
         }, [recur(depth+1, state)]),
       h('g',{transform: 
-        'translate('+m/6+', -'+n+
-          ') scale(1) rotate(12 '+m+' '+n+')'
+        'translate(0,0) scale(1) rotate(225 '+x+' '+y+')'
         }, [recur(depth+1, state)])
       ]
     )
 }
 function update(){
-  //loop.state.width = loop.state.width + 1;
-  //loop.state.height = loop.state.height + 3;
   loop.state.time = Date.now();
   loop.update(loop.state); 
 }
