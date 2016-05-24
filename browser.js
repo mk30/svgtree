6aa69fa7e210cf5a9e136b97f09e180c70a6ffec
Marina Kukso <marina.kukso@gmail.com>
2016-05-24 16:06:00 +0200

    Revert "merging mkdevbranch into master"
    This reverts commit 62066daa5b25333f28070927d6fd63bdd7a33311, reversing
    changes made to dac12ec21858b01cab8875626206edc80113aeba.

--- browser.js
+++ browser.js
@@ -4,6 +4,8 @@ var h = require('virtual-hyperscript-svg');
 var main = require("main-loop");
 
 var initState = {
+  width: 20,
+  height: 60,
   inittime: Date.now(),
   time: Date.now()
 }
@@ -12,13 +14,50 @@ document.body.appendChild(loop.target)
 
 function render (state){
   return h('svg', { width: '100%', height: '100%' }, [
-    recur(0, state, 0)
+    recur(0, state)
   ]);
 }
 
-
+function recur (depth, state) {
+  var currentdepth = Math.min(5, (state.time - state.inittime)/1000)
+  var n = 100*currentdepth
+  if (depth >= 5){
+    return '' 
+  }
+  /*
+  var height = state.height;
+  if (depth == currentdepth -1){
+    //height = height*(depth/5)
+    //recur(depth+1, state)
+      height = state.height
+  }
+  */
+  return  h('g', {
+  }, [
+    h('rect', { 
+      fill: 'none',
+      stroke: 'olive',
+      x: 400 - ((state.time - state.inittime)/1000*(currentdepth/5)), 
+      y: 400 - ((state.time - state.inittime)/1000*(currentdepth/5)), 
+      //y: state.height + 300,
+      width: state.width, 
+      height: Math.max(0, (state.time -
+      state.inittime)/2000*state.height),
+    }), 
+    h('g',{transform: 
+      'translate(-'+state.width+',-'+ state.height + ') scale(1) rotate(-25 225 200)'
+      }, [recur(depth+1, state)]),
+    h('g',{transform: 
+      'translate('+state.width+', -'+state.height+') scale(1) rotate(25 225 200)'
+      }, [recur(depth+1, state)])
+    ]
+  )
+}
 function update(){
+  //loop.state.width = loop.state.width + 1;
+  //loop.state.height = loop.state.height + 3;
   loop.state.time = Date.now();
   loop.update(loop.state); 
+  //console.log((loop.state.time - loop.state.inittime)/5000);
 }
-setInterval(update,50);
+window.setInterval(update,50);
